5aac6d8e6839c7ad4c381b19f276f4b76503e710
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-07-09 14:23:33 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const population = [
+    ...document.querySelectorAll("span.population")
+];
+const total = [];
+for (const el of population){
+    const text = el.textContent;
+    const number = text.replace(/,/g, "");
+    total.push(number);
+}
+const sum = total.reduce((acc, el)=>Number(acc) + Number(el));
+const averege = sum / 10;
+const textSum = document.querySelector("span.total-population");
+const textAverege = document.querySelector("span.average-population");
+textSum.textContent = sum.toLocaleString("en-US");
+textAverege.textContent = averege.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
